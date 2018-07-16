b037f68ee6bb6c12a236bfdb427f418e0990ca45
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-07-16 09:08:58 +0430

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -5,29 +5,10 @@
 #include<string>
 //11
 using namespace std;
-bool guidence(long int a[][9]) {
-	
-	int checks[10] = { 1,1,2,3,4,5,6
-
-	for (int i = 0; i<9; i++) {
-		for (int j = 0; j<9; j++) {
-			for (int o = 0; o<10; o++) {
-				if (o == 0) {
 					
 					checks[o] = 10;
 					check1[o] = 10;
 					checkr[o] = 10;
-				}
-				else {
-					check1[o] = o;
-					checks[o] = o;
-					checkr[o] = o;
-				}
-			}
-
-
-
-			
 					for (int y = 0; y<3; y++)
 						if (a[x][y] != 0)
 							check1[a[x][y]] = 10;
@@ -53,16 +34,6 @@ bool guidence(long int a[][9]) {
 		cout << "no guidence is possible";
 	return 1;
 }
-
-	
-	}
-	//ta inja check kardane soton va radif ha tamam shod hala mirim vase moraba ha
-	//dar fore payin baz neshani mikonim
-	for (int k = 0; k < 10; k++)
-		check1[k] = k;
-
-	for (int i = 0; i <= 6; i = i + 3) {
-		for (int j = 0; j <= 6; j = j + 3) {
 			for (int x = i; x<i + 3; x++) {
 				for (int y = j; y<j + 3; y++) {
 					check1[a[x][y]] = 0;
@@ -82,31 +53,8 @@ bool guidence(long int a[][9]) {
 		3, 4, 5, 2, 8, 6, 1, 7, 9 }, k[9][9];
 	bool c, komaki = false;
 	//hala mikhahim pak konim
-	unsigned seed = time(NULL);
-	srand(seed);
-	cout << "chand adad mikhahid hazf konid?\n";
-	cin >> n;
-	for (int i = 0; i<n; i++) {
-		else i--;
-		cout << rr << rs;
-	}
-	for (int i = 0; i<9; i++)
-		for (int j = 0; j<9; j++)
-		cout << "\ndo you want guidence?\n";
-		cin >> answer;
 		if (answer == "yes")
 			c = guidence(a);
 
 				cout << "enter the position you want to erase\n";
-				cin >> x >> y;
-				if (k[x][y] == 0) {
-					a[x][y] = 0;
-					komaki = true ;
-				}
-				else cout << " shoma ejaze nadarid in adad ra pak konid chon male jadvale asli ast\n ";
-			}
-		komaki = false;
-
-	}
-	cout << " you won ";
-
+				cin >> 
