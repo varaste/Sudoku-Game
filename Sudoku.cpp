8fd34f9dab35fb990e4b4b3727cb51dcc81a7850
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-07-19 22:40:55 +0430

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -16,45 +16,15 @@ using namespace std;
 			if (j<3 && i>5)+)
 					for (int y = 6; y<9; y++)
 						if (a[x][y] != 0)
-							check1[a[x][y]] = 10;
-
-			if (j>5 && i>2 && i<6)
-				for (int x = 3; x<6; x++)
-					for (int y = 6; y<9; y++)
-						if (a[x][y] != 0)
-							check1[a[x][y]] = 10;
-
-			if (j>5 && i>5)
-
-
-			if (p == 1 && s == 1 && r == 1 && a[i][j] == 0) {
-				cout << "\nguidence:you can add num
-
 	if (v == false)
 		cout << "no guidence is possible";
 	return 1;
 }
 			for (int x = i; x<i + 3; x++) {
-				for (int y = j; y<j + 3; y++) {
-					check1[a[x][y]] = 0;
-				}
-			}
-			for (int k = 0; k < 10; k++)
 				if (check1[k] != 0)
 					return false;
 
 		6, 7, 2, 1, 9, 5, 3, 4, 8 ,
 		1, 9, 8, 3, 4, 2, 5, 6, 7 ,
-		8, 5, 9, 7, 6, 1, 4, 2, 3 ,
-		4, 2, 6, 8, 5, 3, 7, 9, 1 ,
-		7, 1, 3, 9, 2, 4, 8, 5, 6 ,
-		9, 6, 1, 5, 3, 7, 2, 8, 4 ,
-		2, 8, 7, 4, 1, 9, 6, 3, 5 ,
-		3, 4, 5, 2, 8, 6, 1, 7, 9 }, k[9][9];
-	bool c, komaki = false;
-	//hala mikhahim pak konim
-		if (answer == "yes")
-			c = guidence(a);
-
-				cout << "enter the position you want to erase\n";
+		8, 5, 9, 7, 6, 1, 4, 2, 3 ,e\n";
 				cin >> 
